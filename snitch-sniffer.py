6d16ee0d15ff7d6e60f9cdd318fb053bc3aa0f51
= <=>
2024-10-10 17:19:55 -0600

    finish find function

---
+++ snitch-sniffer.py
@@ -0,0 +1,4 @@
+def find_snitch():
+	print("This function doesn't actually find snitches, and this project does not actually track them.")
+	print("As a matter of fact, I haven't even programmed a 'long range ultrasonic sensor' before!"
+)
