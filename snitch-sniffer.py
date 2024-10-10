47508662f60d3648bddbaed4bc490cfb59652b9a
= <=>
2024-10-10 17:21:42 -0600

    delete find function

--- snitch-sniffer.py
+++ snitch-sniffer.py
@@ -1,4 +1,2 @@
 def find_snitch():
-	print("This function doesn't actually find snitches, and this project does not actually track them.")
-	print("As a matter of fact, I haven't even programmed a 'long range ultrasonic sensor' before!"
-)
+	print("Oh no! I've 'accidentally' deleted all the contents of this function!")
